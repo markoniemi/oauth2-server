81d48b7c5143aaba52feba626e05dc2da31e4211
Marko Niemi <marko.niemi@gofore.com>
2026-08-15 21:10:45 +0300

    Revert "Fix Spring Boot 4.0.3 test configuration; MockMvc auto-configured with @SpringBootTest"
    This reverts commit 1015fa7302b7ff1d700a34562c251c641f0f8747.

--- src/test/java/com/example/auth/AuthServerIT.java
+++ src/test/java/com/example/auth/AuthServerIT.java
@@ -18,6 +18,7 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class AuthServerIT {
 
     @LocalServerPort
